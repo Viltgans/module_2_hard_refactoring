a8dac2e5fe9a21d929b5fe134c86702de7d9b2d6
Viltgans <80921674+Viltgans@users.noreply.github.com>
2024-08-13 15:24:52 +0300

    Update module_2_hard_refactoring.py

--- module_2_hard_refactoring.py
+++ module_2_hard_refactoring.py
@@ -1,7 +1,7 @@
 import random
 
-numbers = list(range(1, 21))
-random_numbers = list(range(3, 21))
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+random_numbers = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 rock_1 = random.choice(random_numbers)
 rock_2 = []
 summa_list = []
