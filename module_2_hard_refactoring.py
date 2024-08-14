7e6a389e1e2a68abb4bf9eed239ddf40bec75d90
Viltgans <80921674+Viltgans@users.noreply.github.com>
2024-08-14 23:18:32 +0300

    Update module_2_hard_refactoring.py

--- module_2_hard_refactoring.py
+++ module_2_hard_refactoring.py
@@ -1,7 +1,7 @@
 import random
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-random_numbers = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+numbers = list(range(1,21))
+random_numbers = list(range(3,21))
 rock_1 = random.choice(random_numbers)
 rock_2 = []
 summa_list = []
@@ -10,40 +10,27 @@ list_for_3 = []
 list_for_4 = []
 list_for_5 = []
 list_for_6 = []
-sup_list = []
-sup_list_2 = []
-sup_list_3 = []
-sup_list_4 = []
-sup_list_5 = []
-sup_list_6 = []
 
 
 def condition():
     for i in numbers:
         for j in numbers:
-            terms_for_2 = 2
-            terms_for_3 = 3
-            terms_for_4 = 4
-            terms_for_5 = 5
-            terms_for_6 = 6
-            summa = i + j
-            div = rock_1 / summa
-            if rock_1 == summa and i != j:
+            if rock_1 == (i + j) and i != j:
                 summa_list.extend([i, j])
                 continue
-            if div == terms_for_2 and i != j:
+            if (rock_1 / (i + j)) == 2 and i != j:
                 list_for_2.extend([i, j])
                 continue
-            if div == terms_for_3 and i != j:
+            if (rock_1 / (i + j)) == 3 and i != j:
                 list_for_3.extend([i, j])
                 continue
-            if div == terms_for_4 and i != j:
+            if (rock_1 / (i + j)) == 4 and i != j:
                 list_for_4.extend([i, j])
                 continue
-            if div == terms_for_5 and i != j:
+            if (rock_1 / (i + j)) == 5 and i != j:
                 list_for_5.extend([i, j])
                 continue
-            if div == terms_for_6 and i != j:
+            if (rock_1 / (i + j)) == 6 and i != j:
                 list_for_6.extend([i, j])
                 continue
     return (summa_list,
@@ -53,26 +40,36 @@ def condition():
             list_for_5,
             list_for_6)
 
-result = condition()
-sup_list = list(dict.fromkeys(summa_list))
-sup_list_2 = list(dict.fromkeys(list_for_2))
-sup_list_3 = list(dict.fromkeys(list_for_3))
-sup_list_4 = list(dict.fromkeys(list_for_4))
-sup_list_5 = list(dict.fromkeys(list_for_5))
-sup_list_6 = list(dict.fromkeys(list_for_6))
-rock_2 = (sup_list_6 +
-          sup_list_5 +
-          sup_list_4[0:2] +
-          sup_list_3[0:2] +
-          sup_list_2[0:2] +
-          sup_list[0:2] +
-          sup_list_4[2:] +
-          sup_list_3[2:4] +
-          sup_list_2[2:4] +
-          sup_list[2:4] +
-          sup_list_2[4:6] +
-          sup_list[4:6] +
-          sup_list_2[6:] +
-          sup_list[6:])
+
+def duplicate(old_list):
+    for item in old_list:
+        if item in old_list:
+            old_list.remove(item)
+    old_list.reverse()
+    return old_list
+
+
+condition()
+duplicate(summa_list)
+duplicate(list_for_2)
+duplicate(list_for_3)
+duplicate(list_for_4)
+duplicate(list_for_5)
+duplicate(list_for_6)
+
+rock_2 = (list_for_6 +
+          list_for_5 +
+          list_for_4[0:2] +
+          list_for_3[0:2] +
+          list_for_2[0:2] +
+          summa_list[0:2] +
+          list_for_4[2:] +
+          list_for_3[2:4] +
+          list_for_2[2:4] +
+          summa_list[2:4] +
+          list_for_2[4:6] +
+          summa_list[4:6] +
+          list_for_2[6:] +
+          summa_list[6:])
 print("Шифр:", rock_1)
 print("Пароль:", ''.join(str(x) for x in rock_2))
